c7a5899ed2f18e9409b4f3dac34eda6588056b61
LIU Hui <atliuhui@outlook.com>
2026-05-06 12:25:02 +0800

    Refactor command-line options and remove unused dependencies

--- Sync/Program.cs
+++ Sync/Program.cs
@@ -1,13 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using CommandLine;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using Sync.Extensions;
-using Sync.Options;
 using Sync.Services;
+using System.CommandLine;
 
 var builder = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration((context, config) =>
@@ -28,39 +27,78 @@ var builder = Host.CreateDefaultBuilder(args)
     });
 var host = builder.Build();
 
+// https://learn.microsoft.com/zh-cn/dotnet/csharp/fundamentals/tutorials/system-command-line
+var actionOption = new Option<DeviceAction>("--action", "-a")
+{
+    Description = "None, Scan, Sync",
+#if DEBUG
+    DefaultValueFactory = _ => DeviceAction.Scan,
+#else
+    Required = true,
+#endif
+};
+var nameOption = new Option<string>("--name", "-n")
+{
+    Description = "Device Name",
+#if DEBUG
+    DefaultValueFactory = _ => "Redmi 5 Plus",
+#else
+    DefaultValueFactory = _ => string.Empty,
+#endif
+};
+var rootOption = new Option<string>("--root", "-r")
+{
+    Description = "Device Root Directory",
+    DefaultValueFactory = _ => string.Empty,
+};
+
+var deviceCommand = new Command("Device", "Device Sync")
+{
+    Options = { actionOption, nameOption, rootOption, },
+};
+
+var rootCommand = new RootCommand("Sync")
+{
+    Options = { actionOption, nameOption, rootOption, },
+    Subcommands = { deviceCommand, },
+};
+
 try
 {
     using var context = new RuntimeContext(services: host.Services);
-    ParseAndRun(args, context);
+    deviceCommand.SetAction(parseResult =>
+    {
+        var action = parseResult.GetValue(actionOption);
+        var name = parseResult.GetValue(nameOption) ?? string.Empty;
+        var root = parseResult.GetValue(rootOption) ?? string.Empty;
+        Run(action, name, root, context);
+    });
+
+    return rootCommand.Parse(args).Invoke();
 }
 catch (Exception ex)
 {
     Console.Error.WriteLine();
     Console.Error.WriteLine($"[FATAL] {ex.GetType().Name}: {ex.Message}");
     Console.Error.WriteLine(ex.StackTrace);
-    Environment.ExitCode = 1;
+    return 1;
 }
 
-static void ParseAndRun(string[] args, RuntimeContext context)
-{
-    Parser.Default.ParseArguments<DeviceOption>(args)
-        .WithParsed(option => Run(option, context));
-}
-static void Run(DeviceOption option, RuntimeContext context)
+static void Run(DeviceAction action, string name, string rootPath, RuntimeContext context)
 {
     var devices = MediaDeviceExtension.FindDevice();
 
-    if (option.Action == DeviceAction.None)
+    if (action == DeviceAction.None)
     {
         devices.Print();
         return;
     }
 
-    using var device = devices.FindDevice(option.DeviceName);
-    using var service = new DeviceService(context, device, option.RootPath);
-    using var meter = new ActionMeter($"{service.SerialNumber}[{option.DeviceName}] {option.Action}");
+    using var device = devices.FindDevice(name);
+    using var service = new DeviceService(context, device, rootPath);
+    using var meter = new ActionMeter($"{service.SerialNumber}[{name}] {action}");
 
-    Execute(option.Action, service, meter);
+    Execute(action, service, meter);
     context.ArchiveIndex(service.SerialNumber);
 }
 static void Execute(DeviceAction action, DeviceService service, ActionMeter meter)
@@ -76,4 +114,11 @@ static void Execute(DeviceAction action, DeviceService service, ActionMeter mete
     {
         Console.Write($"\r{meter.ConsoleFormat(text)}");
     });
+}
+
+enum DeviceAction
+{
+    None,
+    Scan,
+    Sync,
 }
--- Test/DebugTest.cs
+++ Test/DebugTest.cs
@@ -1,65 +1,11 @@
-﻿using CoenM.ImageHash.HashAlgorithms;
-using MediaDevices;
-using MediaInfo;
-using Microsoft.Extensions.Logging.Abstractions;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
+﻿using MediaDevices;
 using Sync.Extensions;
-using System.IO.Hashing;
 
 namespace Test
 {
     [TestClass]
     public sealed class DebugTest
     {
-        [TestMethod]
-        public void MediaInfoWrapper_Normal()
-        {
-            var user = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            var path = Path.Combine(user, @"Downloads\离别开出花-伴奏-0140-剪裁.mp4");
-            using var stream = File.OpenRead(path);
-            var media = new MediaInfoWrapper(stream, NullLogger<MediaInfoWrapper>.Instance);
-
-            Console.WriteLine($"Camera = {media.Codec}");
-            Console.WriteLine($"Duration = {media.Duration}");
-        }
-        [TestMethod]
-        public void ReadMetadata_Normal()
-        {
-            var user = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            var path = Path.Combine(user, @"OneDrive\图片\20241025\20241028152413.jpg");
-            using var stream = File.OpenRead(path);
-            var meta = MetadataExtractor.ImageMetadataReader.ReadMetadata(stream);
-
-            var tags = meta.SelectMany(item => item.Tags);
-            Console.WriteLine($"Width = {tags.FirstOrDefault(item => item.Name == "Image Width")?.Description}");
-            Console.WriteLine($"Height = {tags.FirstOrDefault(item => item.Name == "Image Height")?.Description}");
-        }
-        [TestMethod]
-        public void ImageHash_Normal()
-        {
-            var user = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            var path = Path.Combine(user, @"OneDrive\图片\20241025\20241028152413.jpg");
-            using var stream = File.OpenRead(path);
-            var algorithm = new AverageHash();
-            var hash = algorithm.Hash(Image.Load<Rgba32>(stream));
-            var code = hash.ToString("X16");
-
-            Console.WriteLine(code);
-        }
-        [TestMethod]
-        public void FileHash_Normal()
-        {
-            var user = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            var path = Path.Combine(user, @"OneDrive\图片\20241025\20241028152413.jpg");
-            using var stream = File.OpenRead(path);
-            var algorithm = new XxHash64();
-            algorithm.Append(stream);
-            var hash = algorithm.GetCurrentHashAsUInt64();
-            var code = hash.ToString("X16");
-
-            Console.WriteLine(code);
-        }
         [TestMethod]
         public void EnumerateFiles_Normal()
         {
